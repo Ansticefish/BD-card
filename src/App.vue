f3b907d5ded7972f4347782ebba20a9857b0530d
Ansticefish <ansticefish0106@gmail.com>
2022-05-29 22:20:12 +0800

    'Set up router'

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <nav>test</nav>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-@import url("https://use.typekit.net/uxs3wyn.css");
-
-html, body {
-  margin: 0;
-  padding: 0;
-  width: 100vw;
-  height: 100vh;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
 
+<style lang="scss">
+ @import url("https://use.typekit.net/uxs3wyn.css");
+
+ html, body {
+    margin: 0;
+    padding: 0;
+    font-size: 100%;
+    font-family: chaloops, sans-serif;
+    vertical-align: baseline;
+    box-sizing: border-box;
+ }
 </style>
